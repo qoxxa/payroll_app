d2de2b583ec1377aea2e9ae9376bda9e279ed9f0
qoxa <lkostya.p@gmail.com>
2026-07-01 16:13:56 +0300

    Добавлен скриншот, исправлена ошибка

--- client/ui.py
+++ client/ui.py
@@ -63,9 +63,9 @@ class PayrollAppUI:
         tk.Label(self.root, text="Начисления:").grid(row=6, column=0, padx=10, pady=5)
         self.accrual_entry = tk.Entry(self.root)
         self.accrual_entry.grid(row=6, column=1, padx=10, pady=5)
-        accrual_type_var = tk.StringVar(self.root)
-        accrual_type_var.set("Бонус")  # Значение по умолчанию
-        accrual_type_menu = ttk.Combobox(self.root, textvariable=accrual_type_var,
+        self.accrual_type_var = tk.StringVar(self.root)
+        self.accrual_type_var.set("Бонус")  # Значение по умолчанию
+        accrual_type_menu = ttk.Combobox(self.root, textvariable=self.accrual_type_var,
                                          values=["Бонус", "Премия", "Компенсация"])
         accrual_type_menu.grid(row=7, column=1, padx=10, pady=5)
 
